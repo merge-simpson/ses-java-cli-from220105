266317cba8a6b4d0110d6b32053a996a1dcc388f
merge-simpson <letsdev@kakao.com>
2023-01-05 21:20:22 +0900

    fix: use array list

--- src/com/example/enroll/manager/EnrollManager.java
+++ src/com/example/enroll/manager/EnrollManager.java
@@ -5,6 +5,9 @@ import com.example.enroll.vo.Reader;
 import com.example.enroll.vo.Speakable;
 import com.example.enroll.vo.Worker;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class EnrollManager {
     /** 지금 요구된 건 Speakable 객체들만 담음.
      * <h3>요구사항</h3>
@@ -13,34 +16,38 @@ public class EnrollManager {
      *     <li>변경/삭제 없이 '등록'만 있음.</li>
      * </ul>
      * */
-//    private List<Speakable> speakableList; // -> 확장 고려한 설계
-    private Speakable[] speakableList; // -> 확장 그딴 거 없이 노빠꾸 설계
-    private int index; // 아무것도 안 주면 멤버변수는 기본값: 0, false, null, ...
+    private List<Speakable> speakableList; // -> 확장 고려한 설계
+    private final int limitSeat;
 
     public EnrollManager() {
         super(); // 생략 가능
-        this.speakableList = new Speakable[2];
-        this.index = 0;
+        this.speakableList = new ArrayList<>(2); // 메모리 2칸만 할당하고 시작
+        this.limitSeat = 2;
+    }
+
+    public EnrollManager(int limitSeat) {
+        this.speakableList = new ArrayList<>(limitSeat);
+        this.limitSeat = limitSeat;
     }
 
     public boolean registerReader(String name) {
         // 유효성 체크: 더 이상 넣을 수 없을 때.
-        if(index >= speakableList.length) return false;
+        if(speakableList.size() > this.limitSeat) return false;
 
         Speakable newMember = new Reader(name);
-        speakableList[index] = newMember;
-        index++;
+        speakableList.add(newMember);
+//        speakableList[index] = newMember;
+//        index++;
 
         return true;
     }
 
     public boolean registerWorker(String name) {
         // 유효성 체크: 더 이상 넣을 수 없을 때.
-        if(index >= speakableList.length) return false;
+        if(speakableList.size() > this.limitSeat) return false;
 
         Speakable newMember = new Worker(name);
-        speakableList[index] = newMember;
-        index++;
+        speakableList.add(newMember);
 
         return true;
     }
@@ -53,6 +60,6 @@ public class EnrollManager {
     }
 
     public int getRest() {
-        return speakableList.length - index;
+        return this.limitSeat - speakableList.size();
     }
 }
